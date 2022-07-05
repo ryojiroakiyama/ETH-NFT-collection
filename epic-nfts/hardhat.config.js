a595f1e4b8c5337b957b84410a3802167bae42d3
rakiyama <ryojiro25@gmail.com>
2022-07-05 19:13:38 +0900

    ethscan apikey追加

--- epic-nfts/hardhat.config.js
+++ epic-nfts/hardhat.config.js
@@ -1,16 +1,15 @@
+require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.4",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_APIKEY
+  },
   networks: {
     rinkeby: {
       url: process.env.STAGING_ALCHEMY_KEY,
-      accounts: [process.env.PRIVATE_KEY],
-    },
-    mainnet: {
-      chainId: 1,
-      url: process.env.PROD_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
